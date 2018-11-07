75715571724deb5eaf09f85b7b4b25300b644888
usercx <1058718750@qq.com>
2018-11-07 15:03:31 +0800

    refactor(index.js): change page load to domcontentloaded
    change waitUntil from networkidel2 to domcontentloaded

--- index.js
+++ index.js
@@ -3,30 +3,37 @@ const dealPage = require("./dealPage.js");
 
 let browser = null;
 
-const pageLength = 39;
+const pageLength = 40;
 
 // 做初始化
 let init = async function(){
-  browser = await puppeteer.launch();
+  browser = await puppeteer.launch({
+    headless: false,
+    timeout: 0,
+  });
 };
 
 let mainfun = async function(){
   await init();
-  const page = await browser.newPage();
 
   (async () => {
-    for(let i = 28; i <= pageLength; i++){
+    for(let i = 1; i <= pageLength; i++){
       let url = `http://jandan.net/ooxx/page-${i}#comments`;
       console.log(`正在爬取第${i}页的网址：${url}`);
+      let page = null;
       try{
-        await page.goto(url, {waitUntil: 'networkidle2'});
+        page = await browser.newPage();
+        await page.goto(url, {timeout: 0, waitUntil: "domcontentloaded"});
+        console.log("load");
         await dealPage(page);
       } catch(e){
+        console.log(e);
         console.log(`第${i}页抓取失败！`);
+      } finally{
+        await page.close();
       }
     }
     console.log("all is over");
-    await page.close();
     await browser.close();
   })();
 };
